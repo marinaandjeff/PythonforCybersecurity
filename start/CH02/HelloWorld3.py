46d8a061a2fcd3e4438c1869050823da79935493
marinaandjeff <mcgee.marina@student.greenriver.edu>
2022-04-16 13:12:49 -0700

    HW 3 update

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
-# A more complex "Hello World" script in python with Inputs
-# Created 
+# A simple "Hello World" script in python with Inputs
+# Created by Marina on April 15 22
+
+#Ask user for their name
+user_name = input("What is your name? ")
+
+# Print hello and their name
+print("Hello {0}".format(user_name ))
+print(f"Hello {user_name}")
+print("Hello " + user_name)
+print("hello", user_name)
+message = "Hello " +user_name
+print(message)
